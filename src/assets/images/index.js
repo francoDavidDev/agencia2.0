5a05cf92f82321e2b5b66da534e26ac43790a3f1
franco david gomez <francodavid_dev@outlook.com>
2024-12-23 15:49:59 -0300

    comprimi y elimine eimagenes

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,4 @@
-import bigShoe1 from './big-shoe1.png'
-import bigShoe2 from './big-shoe2.png'
-import bigShoe3 from './big-shoe3.png'
+
 
 import customer1 from './roro.jpg'
 import customer2 from './david.jpg'
@@ -10,16 +8,8 @@ import headerLogo from './logo.png'
 
 import offer from './offer.svg'
 
-import shoe4 from './shoe4.svg'
-import shoe5 from './shoe5.svg'
-import shoe6 from './shoe6.svg'
-import shoe7 from './shoe7.svg'
-import shoe8 from './shoe8.svg'
 
-import thumbnailBackground from './thumbnail-background.svg'
-import thumbnailShoe1 from './thumbnail-shoe1.svg'
-import thumbnailShoe2 from './thumbnail-shoe2.svg'
-import thumbnailShoe3 from './thumbnail-shoe3.svg'
+
 
 import pageBussines from '../works/famf.png'
 
@@ -36,9 +26,7 @@ import logo from './logo.png'
 
 
 export {
-    bigShoe1,
-    bigShoe2,
-    bigShoe3,
+ 
 
     customer1,
     customer2,
@@ -47,16 +35,9 @@ export {
     headerLogo,
 
     offer,
-    shoe4,
-    shoe5,
-    shoe6,
-    shoe7,
-    shoe8,
+ 
 
-    thumbnailBackground,
-    thumbnailShoe1,
-    thumbnailShoe2,
-    thumbnailShoe3,
+   
     brandingImg,
     pageBussines,
 
